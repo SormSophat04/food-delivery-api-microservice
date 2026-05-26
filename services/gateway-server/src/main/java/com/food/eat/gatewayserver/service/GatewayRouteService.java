3e0fab764c3c0ed5737ef310351e598e1f81a8a4
sorm_sophat04 <you@example.com>
2026-05-26 17:05:00 +0700

    feat: update gateway-server to use Helm v3 and remove deprecated configurations

--- services/gateway-server/src/main/java/com/food/eat/gatewayserver/service/GatewayRouteService.java
+++
@@ -1,9 +0,0 @@
-package com.food.eat.gatewayserver.service;
-
-import com.food.eat.gatewayserver.dto.RouteApiRequest;
-import com.food.eat.gatewayserver.dto.RouteApiResponse;
-import reactor.core.publisher.Mono;
-
-public interface GatewayRouteService {
-     void refreshRoutes();
-}
