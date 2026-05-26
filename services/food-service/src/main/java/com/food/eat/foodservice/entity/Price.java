08ca9d2d98eb5bbbdbf9c79473ed6d0b34ed6907
sorm_sophat04 <you@example.com>
2026-05-26 17:05:16 +0700

    feat: enhance food service entity with Price and FoodStatus relationships and update service configurations

--- services/food-service/src/main/java/com/food/eat/foodservice/entity/Price.java
+++ services/food-service/src/main/java/com/food/eat/foodservice/entity/Price.java
@@ -1,4 +1,17 @@
 package com.food.eat.foodservice.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Price {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "price_id")
+    private Long priceId;
+
+    private String originalPrice;
+
+    private String discountPrice;
 }
